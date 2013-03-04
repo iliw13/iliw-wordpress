6a69314ad461a31f1a6381540d1de965dc516b60
conatus <alex@recordsonribs.com>
2013-03-04 21:43:57 +0000

    No need for explicit definition.

--- wp-config.php
+++ wp-config.php
@@ -19,8 +19,6 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
 define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
 
-define( 'UPLOADS', WP_CONTENT_DIR  . '/uploads' );
-
 // ================================================
 // You almost certainly do not want to change these
 // ================================================
